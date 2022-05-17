ad94e9d473d06e79a8f6514b837e521cfda0f009
Rodrigo <rodrigorael53@gmail.com>
2022-05-17 16:32:34 -0300

    add remove button to item

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,24 @@ function App() {
       setList(state => [...state, newItem]);
     }, 500);
   };
+
+  function removeFromList(item: string) {
+    setTimeout(() => {
+      setList(state => state.filter(item => item !== item));
+    }, 500);
+  };
   
   return (
     <>
     <input placeholder="Novo item" value={newItem} onChange={e => setNewItem(e.target.value)} />
       <button onClick={addToList}>Adicionar</button>
       <ul>
-        {list.map(item => <li key={item}>{item}</li>)}
+        {list.map(item => (
+          <li key={item}>
+            {item}
+            <button onClick={() => removeFromList(item)}>Remover</button>
+          </li>
+        ))}
       </ul>
     </>
   )
